af76802b039f27a209daa17c1ec263d350548a20
Davi Castro <davialmeida47jd@gmail.com>
2024-03-17 22:00:13 -0300

    feat(models): created models to task app and migration file

--- taskbaby_back/task_app/models.py
+++ taskbaby_back/task_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskList(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TaskItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} - {self.task_list}'
