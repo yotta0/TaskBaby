1b37c126441130ddf99c5988338cc2a78fa82096
Davi Castro <davialmeida47jd@gmail.com>
2024-03-21 01:10:15 -0300

    feat(back-end): created task item view do handle task items / list

--- taskbaby_back/task_app/views.py
+++ taskbaby_back/task_app/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Local application imports
-from .models import TaskList
-from .serializers import TaskListSerializer
+from .models import TaskList, TaskItem
+from .serializers import TaskListSerializer, TaskItemSerializer
 
 
 class TaskListView(APIView):
@@ -86,6 +86,99 @@ class TaskListView(APIView):
                     'data': serializer.data
                 },
                 status=status.HTTP_200_OK
+            )
+        except Exception:
+            return Response(
+                {
+                    'message': 'An error occurred',
+                    'data': []
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def delete(self, request):
+        try:
+            data = request.data
+
+            task_list = TaskList.objects.get(id=data['id'])
+
+            task_list.delete()
+
+            return Response(
+                {
+                    'message': 'Task list deleted successfully',
+                    'data': []
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception:
+            return Response(
+                {
+                    'message': 'An error occurred',
+                    'data': []
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class TaskItemView(APIView):
+
+    def get(self, request, pk):
+        try:
+            task_list = TaskList.objects.filter(id=pk)
+
+            if task_list:
+                task_items = TaskItemSerializer(TaskItem.objects.filter(task_list=pk), many=True)
+                return Response(
+                    {
+                        'message': 'Task retrieved successfully',
+                        'data': {
+                            'id': pk,
+                            'title': task_list.values_list("title", flat=True)[0],
+                            'task_items': task_items.data
+                        }
+                    },
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {
+                        'message': 'Task list not found',
+                        'data': []
+                    },
+                    status=status.HTTP_404_NOT_FOUND
+                )
+        except Exception:
+            return Response(
+                {
+                    'message': 'An error occurred',
+                    'data': []
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def post(self, request, pk):
+        try:
+            data = request.data
+
+            serializer = TaskItemSerializer(data=data)
+
+            if not serializer.is_valid():
+                return Response(
+                    {
+                        'message': 'Invalid data',
+                        'data': serializer.errors
+                    },
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
+
+            return Response(
+                {
+                    'message': 'Task list created successfully',
+                    'data': serializer.data
+                },
+                status=status.HTTP_201_CREATED
             )
         except Exception:
             return Response(
