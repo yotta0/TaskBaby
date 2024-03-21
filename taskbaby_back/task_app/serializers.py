1aa982c1b4b09fe5dcaa7b2f232d37ad73308d8f
Davi Castro <davialmeida47jd@gmail.com>
2024-03-21 01:10:54 -0300

    feat(back-end): created task itens serializers to handle task items data

--- taskbaby_back/task_app/serializers.py
+++ taskbaby_back/task_app/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 
 # Local application imports
-from .models import TaskList
+from .models import TaskList, TaskItem
 
 
 class TaskListSerializer(serializers.Serializer):
@@ -20,3 +20,23 @@ class TaskListSerializer(serializers.Serializer):
         )
         task_list.save()
         return task_list
+
+
+class TaskItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField(max_length=200)
+    created_at = serializers.DateTimeField(read_only=True)
+    due_date = serializers.DateTimeField()
+    completed = serializers.BooleanField()
+
+    def create(self, validated_data):
+        task_item = TaskItem.objects.create(
+            title=validated_data['title'],
+            description=validated_data['description'],
+            due_date=validated_data['due_date'],
+            completed=validated_data['completed'],
+            task_list=validated_data['task_list']
+        )
+        task_item.save()
+        return task_item
