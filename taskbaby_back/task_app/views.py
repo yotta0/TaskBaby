2c767567446e754c9172be0c90cb4e6ff1aabe0d
Davi Castro <davialmeida47jd@gmail.com>
2024-03-20 23:59:09 -0300

    fix(back-end): Created CRUD in views to manipulate Task List datas

--- taskbaby_back/task_app/views.py
+++ taskbaby_back/task_app/views.py
@@ -4,10 +4,31 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Local application imports
+from .models import TaskList
 from .serializers import TaskListSerializer
 
 
-class TaskListCreateView(APIView):
+class TaskListView(APIView):
+
+    def get(self, request):
+        try:
+            task_lists = TaskListSerializer(TaskList.objects.all(), many=True)
+
+            return Response(
+                {
+                    'message': 'Task lists retrieved successfully',
+                    'data': task_lists.data
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception:
+            return Response(
+                {
+                    'message': 'An error occurred',
+                    'data': []
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     def post(self, request):
         try:
@@ -36,7 +57,65 @@ class TaskListCreateView(APIView):
             return Response(
                 {
                     'message': 'An error occurred',
-                    'data': None
+                    'data': []
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def put(self, request):
+        try:
+            data = request.data
+
+            task_list = TaskList.objects.get(id=data['id'])
+
+            serializer = TaskListSerializer(task_list, data=data)
+
+            if not serializer.is_valid():
+                return Response(
+                    {
+                        'message': 'Invalid data',
+                        'data': serializer.errors
+                    },
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
+
+            return Response(
+                {
+                    'message': 'Task list updated successfully',
+                    'data': serializer.data
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception:
+            return Response(
+                {
+                    'message': 'An error occurred',
+                    'data': []
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def delete(self, request):
+        try:
+            data = request.data
+
+            task_list = TaskList.objects.get(id=data['id'])
+
+            task_list.delete()
+
+            return Response(
+                {
+                    'message': 'Task list deleted successfully',
+                    'data': []
+                },
+                status=status.HTTP_200_OK
+            )
+        except Exception:
+            return Response(
+                {
+                    'message': 'An error occurred',
+                    'data': []
                 },
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
